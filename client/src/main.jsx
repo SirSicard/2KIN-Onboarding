c8d4a521219576966753cd293ab4d63d019f4565
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-20 10:09:00 +0100

    initialized project

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App';
+import { 
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route index element={<LandingPage/>}/>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
