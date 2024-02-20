f6dab5bc978b9938206bc4cc38b9521e08a33f66
EmilNilsson00 <emilnilsson00@icloud.com>
2024-02-20 15:05:46 +0100

    basic nav tillagd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import lightmode from "./styles/lightmode.module.css"
 
 function App() {
   return (
     <>
+    <header className={lightmode}>
+      <Navbar />
+      </header>
+      <main className={lightmode}>
       <Outlet />
+      </main>
     </>
   );
 }
