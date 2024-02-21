0158510744ad058f489ea66768ac24475aad7269
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-21 11:19:12 +0100

    added the basic structure for the landing page's first section

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,21 @@
-import lightmode from "../styles/lightmode.module.css"
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <div>
-
+      <h1>2KIN</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+        officia aut!
+      </p>
+      <Link to="/"><button>BUY NOW</button></Link>
     </div>
-
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
