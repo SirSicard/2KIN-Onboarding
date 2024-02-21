275a18797129896835d69daceb37beb0f88959f9
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-21 12:34:18 +0100

    styling added

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,20 +1,11 @@
-import { Link } from "react-router-dom";
+//components
+import HeroSection from "../components/HeroSection";
 
 function LandingPage() {
   return (
-    <div>
-      <h1>2KIN</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-        officia aut!
-      </p>
-      <Link to="/"><button>BUY NOW</button></Link>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
 
