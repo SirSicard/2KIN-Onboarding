8dc9dee021e57817fb11e95ecbed6741d31c7ed3
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-21 14:22:59 +0100

    added the content for about section

---
+++ client/src/components/AboutSection.jsx
@@ -0,0 +1,18 @@
+function AboutSection() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+        officia aut!
+        </p>
+        <img src="https://via.placeholder.com/400x300" alt="placeholder" />
+    </div>
+  );
+}
+
+export default AboutSection;
