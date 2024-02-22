71a210ae2103cd17364de59396a9576162d337b3
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-22 14:54:47 +0100

    added content layout for the tech spec section

---
+++ client/src/components/TechSpecSection.jsx
@@ -0,0 +1,48 @@
+function TechSpecSection() {
+  return (
+    <div>
+      <h2>Technical Specifications</h2>
+        <div>
+            <h2>tech spec</h2>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+            accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+            molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+            officia aut!
+            </p>
+            <img src="https://via.placeholder.com/600x425" alt="placeholder" />
+        </div>
+        <div>
+            <h2>tech spec</h2>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+            accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+            molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+            officia aut!
+            </p>
+            <img src="https://via.placeholder.com/600x425" alt="placeholder" />
+        </div>
+        <div>
+            <h2>tech spec</h2>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+            accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+            molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+            officia aut!
+            </p>
+            <img src="https://via.placeholder.com/600x425" alt="placeholder" />
+        </div>
+    </div>
+  );
+}
+
+export default TechSpecSection;
