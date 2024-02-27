4593b952ff6e700e1a9ad911da4f5c4b715524b9
SimonLeBuono123 <simon.villoresi@gmail.com>
2024-02-27 11:37:11 +0100

    Implemented  a working dropdown menu when in smaller screens
    Hamburger menu is currently just a button but will be changed to an icon in the future.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import { Link, NavLink } from "react-router-dom";
 import "../styles/navbar.css";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 function Navbar({ onLoginClick }) {
+  const [activeMenu, setActiveMenu] = useState(false);
+  
+  function handleDropdownMenu() {
+    if (activeMenu) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }
+
+
+
   return (
     <nav>
+    <div className="navbar-top">
       <NavLink to="/">
         <h1 className="Logo">2kin</h1>
       </NavLink>
@@ -17,15 +31,25 @@ function Navbar({ onLoginClick }) {
           <span className="nav-login-btn">Login</span>
         </Link>
       </div>
-      <div className="dropdown-menu">
-      <NavLink to={"/shop"}>SHOP</NavLink>
-        <NavLink to="/docs">DOCS</NavLink>
-        <NavLink to={"/apps"}>APPSTORE</NavLink>
-        <NavLink to={"/cart"}>CART</NavLink>
-        <Link href="#" onClick={onLoginClick}>
-          <span className="nav-login-btn">Login</span>
-        </Link>
+      <button onClick={() => handleDropdownMenu()}>hamburger</button>
       </div>
+      {activeMenu ? (
+        <>
+          <div className="dropdown-menu">
+            <NavLink  onClick={() => handleDropdownMenu()}
+             to={"/shop"}>SHOP</NavLink>
+            <NavLink  onClick={() => handleDropdownMenu()}
+            to="/docs">DOCS</NavLink>
+            <NavLink  onClick={() => handleDropdownMenu()}
+            to={"/apps"}>APPSTORE</NavLink>
+            <NavLink  onClick={() => handleDropdownMenu()}
+            to={"/cart"}>CART</NavLink>
+            <NavLink href="#" onClick={onLoginClick}>
+              LOGIN
+            </NavLink>
+          </div>
+        </>
+      ) : null}
     </nav>
   );
 }
