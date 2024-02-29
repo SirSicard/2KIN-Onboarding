c36dcaf7e263b146cc1f9cbbf08d2bd2ada41edb
SimonLeBuono123 <simon.villoresi@gmail.com>
2024-02-29 11:02:15 +0100

    Fixing css and other bugs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import "../styles/navbar.css";
 import PropTypes from "prop-types";
-import {  useState } from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars, faCartShopping} from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function Navbar({ onLoginClick }) {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const handleDashboardClick = () => {
-     navigate('/user/add-product');
-   };
+  const handleDashboardClick = () => {
+    navigate("/user/add-product");
+  };
   const [activeMenu, setActiveMenu] = useState(false);
-  
+
   function handleDropdownMenu() {
     if (activeMenu) {
       setActiveMenu(false);
@@ -19,40 +19,46 @@ function Navbar({ onLoginClick }) {
       setActiveMenu(true);
     }
   }
- 
-
 
   return (
     <nav>
-    <div className="navbar-top">
-      <NavLink to="/">  
-        <h1 className="Logo" onClick={() => setActiveMenu(false)}
-        >2kin</h1>
-      </NavLink>
-      <div className="nav-right">
-        <NavLink to={'/shop'}>SHOP</NavLink>
-        <NavLink to="/docs">DOCS</NavLink>
-        <NavLink to={'/apps'}>APPSTORE</NavLink>
-        <button onClick={handleDashboardClick}>DASHBOARD</button>
-        <Link href="#" onClick={onLoginClick}>
-          <span className="nav-login-btn">Login</span>
-        </Link>
-      </div>
-       <button  onClick={() => handleDropdownMenu()}>
-       <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
-       </button>
+      <div className="navbar-top">
+        <NavLink to="/">
+          <h1 className="Logo" onClick={() => setActiveMenu(false)}>
+            2kin
+          </h1>
+        </NavLink>
+        <div className="nav-right">
+          <NavLink to={"/shop"}>SHOP</NavLink>
+          <NavLink to="/docs">DOCS</NavLink>
+          <NavLink to={"/apps"}>APPSTORE</NavLink>
+          <button onClick={handleDashboardClick}>DASHBOARD</button>
+          <Link href="#" onClick={onLoginClick}>
+            <span className="nav-login-btn">Login</span>
+          </Link>
+          <NavLink to={"/cart"} className={"navbar-cart"}>
+            <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+          </NavLink>
+        </div>
+        <button onClick={() => handleDropdownMenu()}>
+          <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+        </button>
       </div>
       {activeMenu ? (
         <>
           <div className="dropdown-menu">
-            <NavLink  onClick={() => handleDropdownMenu()}
-             to={"/shop"}>SHOP</NavLink>
-            <NavLink  onClick={() => handleDropdownMenu()}
-            to="/docs">DOCS</NavLink>
-            <NavLink  onClick={() => handleDropdownMenu()}
-            to={"/apps"}>APPSTORE</NavLink>
-            <NavLink  onClick={() => handleDropdownMenu()}
-            to={"/cart"}>CART</NavLink>
+            <NavLink onClick={() => handleDropdownMenu()} to={"/shop"}>
+              SHOP
+            </NavLink>
+            <NavLink onClick={() => handleDropdownMenu()} to="/docs">
+              DOCS
+            </NavLink>
+            <NavLink onClick={() => handleDropdownMenu()} to={"/apps"}>
+              APPSTORE
+            </NavLink>
+            <NavLink onClick={() => handleDropdownMenu()} to={"/cart"}>
+              CART
+            </NavLink>
             <NavLink href="#" onClick={onLoginClick}>
               LOGIN
             </NavLink>
