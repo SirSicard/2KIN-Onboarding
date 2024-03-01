85c738f2c1887b77291910b0118d88c2072c5584
EdvinN1 <nedde14@gmail.com>
2024-03-01 13:24:54 +0100

    'active' navbar + hover effect

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,74 +3,71 @@ import "../styles/navbar.css";
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCartShopping, faUserNinja } from "@fortawesome/free-solid-svg-icons";
+
 function Navbar({ onLoginClick }) {
   const navigate = useNavigate();
 
   const handleDashboardClick = () => {
     navigate("/user/add-product");
   };
+
   const [activeMenu, setActiveMenu] = useState(false);
 
   function handleDropdownMenu() {
-    if (activeMenu) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
+    setActiveMenu(!activeMenu);
   }
 
   return (
     <nav>
       <div className="navbar-top">
-        <NavLink to="/">
+        <Link to="/">
           <h1 className="Logo" onClick={() => setActiveMenu(false)}>
             2kin
           </h1>
-        </NavLink>
+        </Link>
         <div className="nav-right">
-          <NavLink to={"/shop"}>SHOP</NavLink>
-          <NavLink to="/docs">DOCS</NavLink>
-          <NavLink to={"/apps"}>APPSTORE</NavLink>
-          <NavLink to={"/cart"} className={"navbar-cart"}>
-            <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+          <NavLink to="/shop" activeClassName="active">SHOP</NavLink>
+          <NavLink to="/docs" activeClassName="active">DOCS</NavLink>
+          <NavLink to="/apps" activeClassName="active">APPSTORE</NavLink>
+          <NavLink to="/cart" className="navbar-icon">
+            <FontAwesomeIcon icon={faCartShopping} />
           </NavLink>
-          <button onClick={handleDashboardClick}>DASHBOARD</button>
+          <Link to="/user/add-product" className="navbar-icon">
+          <FontAwesomeIcon icon={faUserNinja} onClick={handleDashboardClick}/>
+          </Link>
           <Link href="#" onClick={onLoginClick}>
             <span className="nav-login-btn">Login</span>
           </Link>
         </div>
         <div className="mobile-nav-right">
-          <NavLink to={"/cart"} className={"navbar-cart"} 
-           onClick={() => handleDropdownMenu()}>
-            <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+          <NavLink to="/cart" className="navbar-icon" onClick={handleDropdownMenu}>
+            <FontAwesomeIcon icon={faCartShopping} />
           </NavLink>
-          <button onClick={() => handleDropdownMenu()}>
-            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+          <button onClick={handleDropdownMenu}>
+            <FontAwesomeIcon icon={faBars} />
           </button>
         </div>
       </div>
-      {activeMenu ? (
-        <>
-          <div className="dropdown-menu">
-            <NavLink onClick={() => handleDropdownMenu()} to={"/shop"}>
-              SHOP
-            </NavLink>
-            <NavLink onClick={() => handleDropdownMenu()} to="/docs">
-              DOCS
-            </NavLink>
-            <NavLink onClick={() => handleDropdownMenu()} to={"/apps"}>
-              APPSTORE
-            </NavLink>
-            <NavLink onClick={() => handleDropdownMenu()} to={"/user/add-product"}>
-              ACCOUNT
-            </NavLink>
-            <NavLink href="#" onClick={onLoginClick}>
-              LOGIN
-            </NavLink>
-          </div>
-        </>
-      ) : null}
+      {activeMenu && (
+        <div className="dropdown-menu">
+          <NavLink onClick={handleDropdownMenu} to="/shop">
+            SHOP
+          </NavLink>
+          <NavLink onClick={handleDropdownMenu} to="/docs">
+            DOCS
+          </NavLink>
+          <NavLink onClick={handleDropdownMenu} to="/apps">
+            APPSTORE
+          </NavLink>
+          <NavLink onClick={handleDropdownMenu} to="/user/add-product">
+            ACCOUNT
+          </NavLink>
+          <NavLink href="#" onClick={onLoginClick}>
+            LOGIN
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 }
